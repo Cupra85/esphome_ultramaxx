7eafa6364fc320621f1ade357bf11a0e85f9cc88
Cupra85 <daniel.curten@gmail.com>
2025-11-04 19:12:52 +0100

    Update wmz_mbus_custom.h

--- components/wmz_mbus_custom/wmz_mbus_custom.h
+++ components/wmz_mbus_custom/wmz_mbus_custom.h
@@ -1,12 +1,9 @@
 #pragma once
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
+#include "esphome/components/text_sensor/text_sensor.h"
 #include <driver/uart.h>
 #include <vector>
-#include "esphome/components/text_sensor/text_sensor.h"
-
-sensor::TextSensor *debug_frame_ = new sensor::TextSensor();
-
 
 namespace esphome {
 namespace wmz_mbus_custom {
@@ -19,7 +16,7 @@ class WMZComponent : public PollingComponent {
   void update() override;
   float get_setup_priority() const override { return setup_priority::DATA; }
 
-  // Sensor-Pointer (wichtig für ESPHome)
+  // Sensoren
   sensor::Sensor *heat_energy_kwh_ = new sensor::Sensor();
   sensor::Sensor *volume_m3_       = new sensor::Sensor();
   sensor::Sensor *flow_temp_c_     = new sensor::Sensor();
@@ -27,24 +24,26 @@ class WMZComponent : public PollingComponent {
   sensor::Sensor *power_w_         = new sensor::Sensor();
   sensor::Sensor *flow_lh_         = new sensor::Sensor();
 
+  // Debug-Textsensor (Rohdaten)
+  text_sensor::TextSensor *debug_frame_ = new text_sensor::TextSensor();
+
  protected:
   int tx_pin_;
   int rx_pin_;
   uart_port_t uart_num_ = UART_NUM_1;
 
-  // UART Helper
+  // UART helper
   void uart_configure(int baud, uart_parity_t parity);
   void uart_write(const uint8_t *data, size_t len, uint32_t wait_ms = 100);
   int  uart_read(uint8_t *data, size_t maxlen, uint32_t timeout_ms);
 
-  // Kommunikationsablauf
+  // Kommunikationslogik
   void wake_up();                 // 0x55 für 2.2s @ 2400 Bd 8N1
   bool send_init_expect_e5();     // SND_NKE → E5
   void send_req_ud2();            // REQ_UD2 senden
   std::vector<uint8_t> read_frame(uint32_t window_ms);
   void parse_and_publish(const std::vector<uint8_t> &buf);
 
-  // kleine Helfer
   static uint32_t u32le(const uint8_t *p) {
     return uint32_t(p[0]) | (uint32_t(p[1]) << 8) | (uint32_t(p[2]) << 16) | (uint32_t(p[3]) << 24);
   }
