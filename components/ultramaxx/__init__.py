663d55de982c56b480d9590403f860d7a8d597ef
Cupra85 <daniel.curten@gmail.com>
2026-02-15 22:53:08 +0100

    Update __init__.py

--- components/ultramaxx/__init__.py
+++ components/ultramaxx/__init__.py
@@ -1,1 +1,14 @@
 
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart
+
+CODEOWNERS = ["@deinname"]
+DEPENDENCIES = ["uart"]
+ultramaxx_ns = cg.esphome_ns.namespace("ultramaxx")
+
+CONF_ULTRAMAXX_ID = "ultramaxx_id"
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(ultramaxx_ns.class_("UltraMaXXComponent", cg.Component)),
+}).extend(cv.COMPONENT_SCHEMA)
