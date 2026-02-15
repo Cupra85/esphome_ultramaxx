16cb965bfd69b1bac847f91dd56e53dcfee0ab4e
Cupra85 <daniel.curten@gmail.com>
2026-02-15 16:29:33 +0100

    Update dependencies and configuration for sensor.py

--- components/ultramaxx/sensor.py
+++ components/ultramaxx/sensor.py
@@ -3,7 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.const import CONF_ID
 
-AUTO_LOAD = ["uart"]
+DEPENDENCIES = ["uart"]
 
 ultramaxx_ns = cg.esphome_ns.namespace("ultramaxx")
 
@@ -13,19 +13,17 @@ UltraMaXXComponent = ultramaxx_ns.class_(
     uart.UARTDevice,
 )
 
-CONF_ULTRAMAXX_ID = "id"
-
-# 👉 WICHTIG: Das registriert den YAML Block "ultramaxx:"
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(UltraMaXXComponent),
+        cv.Required("uart_id"): cv.use_id(uart.UARTComponent),
     }
-).extend(uart.UART_DEVICE_SCHEMA).extend(cv.COMPONENT_SCHEMA)
+).extend(cv.COMPONENT_SCHEMA)
 
 
 async def to_code(config):
-    uart_parent = await cg.get_variable(config[uart.CONF_UART_ID])
-    var = cg.new_Pvariable(config[CONF_ID], uart_parent)
+    parent = await cg.get_variable(config["uart_id"])
+    var = cg.new_Pvariable(config[CONF_ID], parent)
 
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
