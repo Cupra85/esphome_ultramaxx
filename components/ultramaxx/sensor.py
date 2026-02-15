e07ea8999c272dd7b6812ee10dae989ce396ecdd
Cupra85 <daniel.curten@gmail.com>
2026-02-15 15:23:02 +0100

    Refactor UltraMaXXComponent initialization and config

--- components/ultramaxx/sensor.py
+++ components/ultramaxx/sensor.py
@@ -3,13 +3,19 @@ import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.const import CONF_ID
 
-DEPENDENCIES = ["uart"]
+AUTO_LOAD = ["uart"]
 
 ultramaxx_ns = cg.esphome_ns.namespace("ultramaxx")
-UltraMaXXComponent = ultramaxx_ns.class_("UltraMaXXComponent", cg.Component, uart.UARTDevice)
 
-CONF_ULTRAMAXX_ID = "ultramaxx_id"
+UltraMaXXComponent = ultramaxx_ns.class_(
+    "UltraMaXXComponent",
+    cg.Component,
+    uart.UARTDevice,
+)
 
+CONF_ULTRAMAXX_ID = "id"
+
+# 👉 WICHTIG: Das registriert den YAML Block "ultramaxx:"
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(UltraMaXXComponent),
@@ -18,6 +24,8 @@ CONFIG_SCHEMA = cv.Schema(
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    uart_parent = await cg.get_variable(config[uart.CONF_UART_ID])
+    var = cg.new_Pvariable(config[CONF_ID], uart_parent)
+
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
