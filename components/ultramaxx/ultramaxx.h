b8b9b22db654d9b5e899e9d0d44a4de0e4071d51
Cupra85 <daniel.curten@gmail.com>
2026-02-16 15:26:52 +0100

    Update ultramaxx.h

--- components/ultramaxx/ultramaxx.h
+++ components/ultramaxx/ultramaxx.h
@@ -4,12 +4,16 @@
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/uart/uart.h"
 
 namespace esphome {
 namespace ultramaxx {
 
+// Wir nutzen UARTDevice für die Kommunikation und PollingComponent für das Intervall
 class UltraMaXXComponent : public PollingComponent, public uart::UARTDevice {
  public:
+  // Der Konstruktor bekommt den Parent-UART übergeben (wichtig für S3)
+  UltraMaXXComponent(uart::UARTComponent *parent) : PollingComponent(), uart::UARTDevice(parent) {}
   // ⭐ update_interval kommt aus sensor.py → kein PollingComponent(...)
   UltraMaXXComponent(uart::UARTComponent *parent)
       : uart::UARTDevice(parent) {}
@@ -20,29 +24,27 @@ class UltraMaXXComponent : public PollingComponent, public uart::UARTDevice {
 
   float get_setup_priority() const override { return setup_priority::DATA; }
 
+  // Setter-Methoden für die sensor.py
   // Setter (werden aus sensor.py aufgerufen)
   void set_serial_number_sensor(sensor::Sensor *s) { serial_number_ = s; }
   void set_total_energy_sensor(sensor::Sensor *s) { total_energy_ = s; }
   void set_total_volume_sensor(sensor::Sensor *s) { total_volume_ = s; }
-  void set_current_power_sensor(sensor::Sensor *s) { current_power_ = s; }
-  void set_temp_flow_sensor(sensor::Sensor *s) { temp_flow_ = s; }
-  void set_temp_return_sensor(sensor::Sensor *s) { temp_return_ = s; }
-  void set_temp_diff_sensor(sensor::Sensor *s) { temp_diff_ = s; }
+@@ -30,8 +31,8 @@ class UltraMaXXComponent : public PollingComponent, public uart::UARTDevice {
   void set_meter_time_sensor(text_sensor::TextSensor *s) { meter_time_ = s; }
 
  protected:
+  // Hilfsfunktion zum Dekodieren von BCD-Werten (typisch für Allmess/M-Bus)
+  float decode_bcd(std::vector<uint8_t> &data, size_t start, size_t len);
   // ⭐ const reference (besser)
   float decode_bcd(const std::vector<uint8_t> &data, size_t start, size_t len);
 
   sensor::Sensor *serial_number_{nullptr};
   sensor::Sensor *total_energy_{nullptr};
-  sensor::Sensor *total_volume_{nullptr};
-  sensor::Sensor *current_power_{nullptr};
-  sensor::Sensor *temp_flow_{nullptr};
-  sensor::Sensor *temp_return_{nullptr};
+@@ -42,13 +43,13 @@ class UltraMaXXComponent : public PollingComponent, public uart::UARTDevice {
   sensor::Sensor *temp_diff_{nullptr};
   text_sensor::TextSensor *meter_time_{nullptr};
 
+  // Status-Variablen für die State-Machine in der cpp
   uint8_t state_{0};
   uint32_t state_ts_{0};
   uint32_t wake_start_{0};
@@ -51,5 +53,7 @@ class UltraMaXXComponent : public PollingComponent, public uart::UARTDevice {
   std::vector<uint8_t> rx_buffer_;
 };
 
+}  // namespace ultramaxx
+}  // namespace esphome
 }  
 }
