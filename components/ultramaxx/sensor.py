2fb2c5a5fa236d8052d681cab185b546ed237e7b
Cupra85 <daniel.curten@gmail.com>
2026-02-16 20:48:33 +0100

    Update sensor.py v2

--- components/ultramaxx/sensor.py
+++ components/ultramaxx/sensor.py
@@ -25,6 +25,7 @@ CONFIG_SCHEMA = (
         {
             cv.GenerateID(): cv.declare_id(UltraMaXXComponent),
             cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+
             cv.Optional(CONF_SERIAL_NUMBER): sensor.sensor_schema(),
             cv.Optional(CONF_TOTAL_ENERGY): sensor.sensor_schema(unit_of_measurement="MWh", accuracy_decimals=3),
             cv.Optional(CONF_TOTAL_VOLUME): sensor.sensor_schema(unit_of_measurement="m³", accuracy_decimals=2),
@@ -32,6 +33,7 @@ CONFIG_SCHEMA = (
             cv.Optional(CONF_TEMP_FLOW): sensor.sensor_schema(unit_of_measurement="°C", accuracy_decimals=1),
             cv.Optional(CONF_TEMP_RETURN): sensor.sensor_schema(unit_of_measurement="°C", accuracy_decimals=1),
             cv.Optional(CONF_TEMP_DIFF): sensor.sensor_schema(unit_of_measurement="K", accuracy_decimals=2),
+
             cv.Optional(CONF_METER_TIME): text_sensor.text_sensor_schema(),
         }
     )
@@ -42,21 +44,24 @@ CONFIG_SCHEMA = (
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_UART_ID])
     var = cg.new_Pvariable(config[CONF_ID], parent)
+
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
-    for key, func in [
-        (CONF_SERIAL_NUMBER, "set_serial_number_sensor"),
-        (CONF_TOTAL_ENERGY, "set_total_energy_sensor"),
-        (CONF_TOTAL_VOLUME, "set_total_volume_sensor"),
-        (CONF_CURRENT_POWER, "set_current_power_sensor"),
-        (CONF_TEMP_FLOW, "set_temp_flow_sensor"),
-        (CONF_TEMP_RETURN, "set_temp_return_sensor"),
-        (CONF_TEMP_DIFF, "set_temp_diff_sensor"),
-    ]:
+    mapping = [
+        (CONF_SERIAL_NUMBER, "set_serial_number_sensor", sensor.new_sensor),
+        (CONF_TOTAL_ENERGY, "set_total_energy_sensor", sensor.new_sensor),
+        (CONF_TOTAL_VOLUME, "set_total_volume_sensor", sensor.new_sensor),
+        (CONF_CURRENT_POWER, "set_current_power_sensor", sensor.new_sensor),
+        (CONF_TEMP_FLOW, "set_temp_flow_sensor", sensor.new_sensor),
+        (CONF_TEMP_RETURN, "set_temp_return_sensor", sensor.new_sensor),
+        (CONF_TEMP_DIFF, "set_temp_diff_sensor", sensor.new_sensor),
+    ]
+
+    for key, setter, factory in mapping:
         if key in config:
-            s = await sensor.new_sensor(config[key])
-            cg.add(getattr(var, func)(s))
+            s = await factory(config[key])
+            cg.add(getattr(var, setter)(s))
 
     if CONF_METER_TIME in config:
         ts = await text_sensor.new_text_sensor(config[CONF_METER_TIME])
