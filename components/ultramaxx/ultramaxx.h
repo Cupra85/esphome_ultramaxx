c242498fde299110bbdef74c01187bab76eb836e
Cupra85 <daniel.curten@gmail.com>
2026-02-19 17:53:53 +0100

    Update ultramaxx.h v5.3

--- components/ultramaxx/ultramaxx.h
+++ components/ultramaxx/ultramaxx.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#include <string>
-#include <vector>
-
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/sensor/sensor.h"
@@ -13,7 +10,12 @@ namespace ultramaxx {
 
 class UltraMaXXComponent : public PollingComponent, public uart::UARTDevice {
  public:
-  explicit UltraMaXXComponent(uart::UARTComponent *parent) : uart::UARTDevice(parent) {}
+  UltraMaXXComponent(uart::UARTComponent *parent) : PollingComponent(), uart::UARTDevice(parent) {}
+
+  void setup() override;
+  void update() override;
+  void loop() override;
+  float get_setup_priority() const override { return setup_priority::DATA; }
 
   void set_serial_number_sensor(sensor::Sensor *s) { serial_number_ = s; }
   void set_total_energy_sensor(sensor::Sensor *s) { total_energy_ = s; }
@@ -24,17 +26,10 @@ class UltraMaXXComponent : public PollingComponent, public uart::UARTDevice {
   void set_temp_diff_sensor(sensor::Sensor *s) { temp_diff_ = s; }
   void set_meter_time_sensor(text_sensor::TextSensor *s) { meter_time_ = s; }
 
-  void setup() override;
-  void update() override;
-  void loop() override;
-
  protected:
-  enum UMState : uint8_t { UM_IDLE, UM_WAKEUP, UM_WAIT, UM_SEND, UM_RX };
-
-  float decode_bcd_(const std::vector<uint8_t> &data, size_t start, size_t len) const;
-  uint32_t decode_u_le_(const std::vector<uint8_t> &data, size_t start, size_t len) const;
-  bool decode_cp32_datetime_(const std::vector<uint8_t> &data, size_t start, std::string &out) const;
-  bool extract_long_frame_(const std::vector<uint8_t> &buf, std::vector<uint8_t> &out_frame) const;
+  float decode_bcd(const std::vector<uint8_t> &data, size_t start, size_t len);
+  uint32_t decode_u_le(const std::vector<uint8_t> &data, size_t start, size_t len);
+  bool decode_cp32_datetime_(const std::vector<uint8_t> &data, size_t start, std::string &out);
 
   sensor::Sensor *serial_number_{nullptr};
   sensor::Sensor *total_energy_{nullptr};
@@ -45,12 +40,12 @@ class UltraMaXXComponent : public PollingComponent, public uart::UARTDevice {
   sensor::Sensor *temp_diff_{nullptr};
   text_sensor::TextSensor *meter_time_{nullptr};
 
-  UMState state_{UM_IDLE};
   uint32_t state_ts_{0};
   uint32_t wake_start_{0};
   uint32_t last_send_{0};
   uint32_t last_rx_byte_{0};
   bool fcb_toggle_{false};
+
   std::vector<uint8_t> rx_buffer_;
 };
 
